4ca5d5b9f7754febc798e4cf5634a809d9bc89c0
michael-walker <mwalker009@hotmail.com>
2019-09-16 13:26:28 +1000

    rewrite index.html

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ echo "FRONTEND: "$PROXY_FRONTEND
 sed -i 's/_server_/'"$PROXY_BACKEND"'/g' proxy.js
 sed -i 's/_client_/'"$PROXY_FRONTEND"'/g' proxy.js
 sed -i 's/_port_/'"$PROXY_LISTEN_PORT"'/g' proxy.js
+sed -i 's/<head>/<head><script>window.REACT_APP_DEMO = '"$REACT_APP_DEMO"';<\/script>/g' build/index.html
 nohup node proxy.js proxy.log > output.log &
 serve -s build
